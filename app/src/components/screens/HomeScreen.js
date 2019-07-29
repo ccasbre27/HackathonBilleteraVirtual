0174c460f38cce2584fece27ecf41a7ce89d1065
jamesjara <jamesjara@gmail.com>
2019-07-29 16:42:23 -0600

    aws config

--- app/src/components/screens/HomeScreen.js
+++ app/src/components/screens/HomeScreen.js
@@ -5,8 +5,23 @@ import {
   Text,
 } from 'react-native'
 
+export const isAccountLinked = () => {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem("ACCOUNT_LINKED")
+      .then(res => {
+        if (res !== null) {
+          resolve(res);
+        } else {
+          resolve(false);
+        }
+      })
+      .catch(err => reject(err));
+  });
+};
+
 export default class HomeScreen extends React.Component {
   render() {
+    
     return (
       <View style={styles.container}> 
         <Text style={styles.textStyle}>Home</Text>
