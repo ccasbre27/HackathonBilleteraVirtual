93333a9465cf978ea9aa18c68f609618af1558fa
ccasbre27@hotmail.com <ccasbre27@hotmail.com>
2019-07-30 11:51:12 -0600

    added design of making payment page

--- app/src/components/screens/GetPaymentScreen.js
+++ app/src/components/screens/GetPaymentScreen.js
@@ -3,13 +3,20 @@ import {
   StyleSheet,
   View,
   Text,
+  Image,
+  TextInput
 } from 'react-native'
 
 export default class GetPaymentScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}> 
-        <Text style={styles.textStyle}>Get payment</Text>
+      <View style = { styles.container }>
+          <Text  style={styles.titleText}>Please scan the QR Code</Text>
+          <Image source={require('../images/scan-qr.png')} style={{width: 200, height: 200}} />
+          <View style = { styles.container_inline }>
+              <Image source={require('../images/audio_icon.png')} style={{  width: 75, height: 75 }}/>
+              <Text style={{ fontSize: 17, color: '#fff' }}>  ₡ 2500 </Text> 
+          </View>
       </View>
     )
   }
@@ -26,5 +33,15 @@ const styles = StyleSheet.create({
     fontSize: 18,
     padding: 10,
     color: '#fff'
+  },
+  container_inline: {
+    flexDirection:'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  titleText: {
+      fontSize: 20,
+      marginBottom: 10,
+      color: '#fff'
   }
 })
